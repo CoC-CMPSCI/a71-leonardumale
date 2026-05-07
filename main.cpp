41ca4de40b67c96d80884ff578409384ef26990c
leonardumale <leonardumale@gmail.com>
2026-05-06 19:38:36 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,47 @@
+#include <iostream>
+#include <random>
+#include <iomanip>
 #include "main.hpp"
 
-int main()
-{
-	int number[SIZE];
-	int last;
+using namespace std;
 
-	last = makearray(number);
-	printout(number, last);
-	bubble(number, last);
-	printout(number, last);
-	// for (int i = 0; i < last; i++)
-	// 	bubble(number, last);
-	// printout(number, last);
+int makearray(int number[])
+{
+    random_device rd;
+    mt19937 gen(rd());
+
+    uniform_int_distribution<> sizeRand(10, 20);
+    uniform_int_distribution<> valueRand(0, 99);
+
+    int count = sizeRand(gen);
+
+    for(int i = 0; i < count; i++)
+    {
+        number[i] = valueRand(gen);
+    }
+
+    return count;
+}
+
+void bubble(int number[], int last)
+{
+    for(int i = 0; i < last; i++)
+    {
+        if(number[i] > number[i + 1])
+        {
+            int temp = number[i];
+            number[i] = number[i + 1];
+            number[i + 1] = temp;
+        }
+    }
+}
+
+void printout(int number[], int last)
+{
+    for(int i = 0; i <= last; i++)
+    {
+        cout << number[i] << "\t";
+    }
+
+    cout << endl;
 }
